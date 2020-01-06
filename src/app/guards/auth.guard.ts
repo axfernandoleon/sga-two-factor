07f47b47fbbd8a3365c33c8ae091814f2a0d5c25
Alex Fernando León <axfernandoleon@outlook.com>
2020-01-06 12:18:23 -0500

    init guards v1

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -12,9 +12,9 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.afsAuth.authState
+    return this.afsAuth.authState 
     .pipe(take(1))
-    .pipe(map(authState => !!authState))
+    .pipe(map(authState => !!authState)) 
     .pipe(tap(auth => {
       if (!auth) {
         this.router.navigate(['/user/login']);
